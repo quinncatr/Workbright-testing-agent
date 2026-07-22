a845c1dc03c1c89a339c8613ce31e8b53bd08fec
Colin Stevenson <colin.stevenson@gmail.com>
2026-07-22 16:01:31 -0600

    Configure playwright to use list reporter, silence dotenv notifications

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,14 +3,8 @@ import dotenv from 'dotenv';
 import path from 'node:path';
 dotenv.config({
   path: path.resolve(__dirname, '.env'),
+  quiet: true,
 });
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -26,7 +20,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['html', { open: 'never'}],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
